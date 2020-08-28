cf54e166f159c7c32adb6d6068cf51ecf10ce8d0
Егор Феофанов <dr.egor-feofanov@yandex.ru>
2020-08-28 15:35:09 +0300

    initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# parcel-boilerplate
+
+# Копирование репозитория 
+
+git clone https://github.com/uporot1k/parcel-boilerplate.git
+
+# Установка модулей
+npm i
+
+# Старт проекта
+npm run serve
---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+console.log(1);
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  speed: 500,
+  loop: true,
+  loopPreventsSlide: true,
+  preloadImages: true,
+  loopFillGroupWithBlank: true,
+  disableOnInteraction: false,
+  grabCursor: false,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: { delay: 2000 },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  preloadImages: false,
+  lazy: {
+    loadPrevNext: true,
+    loadOnTransitionStart: true
+  },
+});
